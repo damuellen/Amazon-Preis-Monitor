5311f1509196b6db9a5a1e2335dbda0b694b69c1
Daniel Muellenborn <damuellen@aol.com>
2016-01-15 15:33:47 +0100

    Initial Commit

---
+++ Amazon Preis Monitor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Amazon Preis Monitor
+//
+//  Created by Daniel Müllenborn on 15/01/16.
+//  Copyright © 2016 Daniel Müllenborn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Amazon Preis Monitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Amazon Preis Monitor
+//
+//  Created by Daniel Müllenborn on 15/01/16.
+//  Copyright © 2016 Daniel Müllenborn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
